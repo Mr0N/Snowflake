63a8f9a9a1f1d8c2509d0688dc10b857ad2557a4
maksim2143 <61237140+maksim2143@users.noreply.github.com>
2020-12-06 21:14:30 +0200

    Добавьте файлы проекта.

---
+++ ShowFlake/Work.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Threading;
+using System.Threading.Tasks;
+namespace ShowFlake
+{
+    class Work
+    {
+        public void Start(int count)
+        {
+            Thread thread = new Thread(() =>
+            {
+                var sf = new SF();//WPF окно
+                    sf.ShowDialog();
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+        }
+
+        public Work()
+        {
+        }
+    }
+}
